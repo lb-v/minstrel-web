d65f9122cf9257b866bc7a6fcfdc62cf8ba368f1
bx5a <bx5a.develop@gmail.com>
2016-07-13 14:33:16 +0200

    Initialize git repo from angular boilerplate

---
+++ dev/boot.ts
@@ -0,0 +1,5 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
