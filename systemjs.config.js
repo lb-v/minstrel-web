55e751709470d1293e19e4fd81aec6d5503ceaa3
bx5a <bx5a.develop@gmail.com>
2016-07-28 09:59:37 +0200

    Move systemjs to root folder and make it a js instead of ts

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,6 @@
     var map = {
         'app': 'app',
         '@angular': 'node_modules/@angular',
-        // 'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs': 'node_modules/rxjs'
     };
     // packages tells the System loader how to load when no filename and/or no extension
@@ -15,37 +14,40 @@
         'client': { main: 'app/boot.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+        'app': {defaultExtension: 'js'},
     };
     var ngPackageNames = [
         'common',
         'compiler',
         'core',
+        'http',
         'platform-browser',
         'platform-browser-dynamic',
         'router',
         'router-deprecated',
         'upgrade',
     ];
-    var ngIndexPackageNames = [
-        'http'
-    ];
+
     // Individual files (~300 requests):
     function ngPackageIndex(pkgName) {
         packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
     }
+
     // Bundled (~40 requests):
     function packUmd(pkgName) {
-        if (pkgName == 'router')
-            packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
-        else
-            packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+         packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
     }
+
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
     // Add package entries for angular packages
     ngPackageNames.forEach(packUmd);
-    ngIndexPackageNames.forEach(ngPackageIndex);
+
     var config = {
         map: map,
         packages: packages
     };
+
     System.config(config);
 })(this);
