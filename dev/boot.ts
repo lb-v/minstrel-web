cf4387f917389b92fcde475ef3b19fd817b02014
bx5a <bx5a.develop@gmail.com>
2016-07-29 19:46:24 +0200

    Provide the player factory

--- dev/boot.ts
+++ dev/boot.ts
@@ -4,9 +4,11 @@ import {AppComponent} from "./app.component";
 
 import {HTTP_PROVIDERS} from '@angular/http';
 import {PlaylistService} from "./playlist.service";
+import {PlayerFactory} from "./Player/player.factory";
 
 bootstrap(AppComponent, 
           [ 
             HTTP_PROVIDERS, 
-            provide(PlaylistService, {useValue: new PlaylistService()}) 
+            provide(PlaylistService, {useValue: new PlaylistService()}) ,
+            provide(PlayerFactory, {useValue: new PlayerFactory()})
           ]);
